524e2312d5b36ca97109872fcc282c5f52b2c9d4
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2020-04-21 15:48:18 -0400

    Add files via upload

---
+++ others/cosfuncj.m
@@ -0,0 +1,9 @@
+function j = cosfuncj(x, y, theta)
+  x = [1 1; 1 2; 1 3]
+  y = [1;2;3]
+  theta = [0;1];
+  m = size(x,1)
+  pred = x*theta;
+  sqrerror = (pred-y).^2;
+  j = 1/(2*m) * sum(sqrerror);
+endfunction
---
+++ others/sqandcube.m
@@ -0,0 +1,5 @@
+function [y, z] = sqandcube(x)
+  x= 10
+  z = x^3
+  y = x^2
+endfunction
